dce6f26534baa3bc363b2c0ff237614ee3868de0
FLAVIO RAFAEL CARDOZO <flavero@gmail.com>
2023-11-14 19:42:01 -0300

    ate o 3

---
+++ exercicio03.js
@@ -0,0 +1,9 @@
+//Faça um programa que receba dois numeros inteiro e gere os numeros inteiros que estao no intervalo compreendido por eles
+
+let inicio = 7
+let fim = 25
+
+while(inicio <= fim){
+    console.log(inicio)
+    inicio++
+}
--- node_bb802d1ffbdba.tmp.js
+++ node_bb802d1ffbdba.tmp.js
@@ -5,13 +5,3 @@ while(i <= 50){
     console.log(i)
     i+=2
 }
-
-/*
-let i = 1
-while(i <= 50){
-    if(i%2 != 0){
-        console.log(i)
-    }
-    i++
-}
-*/
