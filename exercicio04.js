7df74626bfb072e297711e249a07a103606883dc
FLAVIO RAFAEL CARDOZO <flavero@gmail.com>
2023-11-14 19:58:51 -0300

    ate o 4

---
+++ exercicio04.js
@@ -0,0 +1,11 @@
+//Altere o programa anterior para mostrar no final a soma dos numeros
+
+let inicio = 10
+let fim = 20
+let soma = 0
+
+while(inicio < fim-1){
+    inicio++
+    console.log(inicio)
+    soma = soma + inicio
+}
---
+++ node_2f1eb2dd461c4.tmp.js
@@ -0,0 +1,9 @@
+//Faça um programa que receba dois numeros inteiro e gere os numeros inteiros que estao no intervalo compreendido por eles
+
+let inicio = 10
+let fim = 20
+
+while(inicio <= fim-1){
+    inicio++
+    console.log(inicio)
+}
